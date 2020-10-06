4fab38fe1f494f1ab3dcab538bdfb12018412ae7
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-10-06 12:16:12 -0700

    Final Submission

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 // The headline element
-const display = document.querySelector('#display')
+const display = document.querySelector("#display");
 
 // The input field for size
-const inputSize = document.querySelector('#input-size')
-// The element that displays the value 
-const showSize = document.querySelector('#show-size')
+const inputSize = document.querySelector("#input-size");
+// The element that displays the value
+const showSize = document.querySelector("#show-size");
 // Repeat this for the input and "show" elements
 
-const inputColor = document.querySelector('#input-color')
+const inputColor = document.querySelector("#input-color");
 
-const inputBackgroundColor = document.querySelector('#input-bg-color')
+const inputBackgroundColor = document.querySelector("#input-bg-color");
 
-inputSize.addEventListener('input', handleInput)
+inputSize.addEventListener("input", handleInput);
 
 function handleInput() {
-	// get the font size:
-	const fontSize = inputSize.value
-	// Set the style 
-	display.style.fontSize = fontSize
-    // Show the size
-    showSize.innerHTML = fontSize
+  // get the font size:
+  const fontSize = inputSize.value;
+  // Set the style
+  display.style.fontSize = fontSize;
+  // Show the size
+  showSize.innerHTML = fontSize;
 
-    // get the font family:
-    const fontFamily = inputFamily.value
-	// Set the family 
-	display.style.fontFamily = fontFamily
-    // Show the family
-    showFamily.innerHTML = fontFamily
+  // get the font family:
+  const fontFamily = inputFamily.value;
+  // Set the family
+  display.style.fontFamily = fontFamily;
+  // Show the family
+  showFamily.innerHTML = fontFamily;
 
-    // get the font color:
-    const fontColor = inputColor.value
-	// Set the color 
-	display.style.fontColor = fontColor
-    // Show the color
-    showColor.innerHTML = fontColor
+  // get the font color:
+  const fontColor = inputColor.value;
+  // Set the color
+  display.style.fontColor = fontColor;
+  // Show the color
+  showColor.innerHTML = fontColor;
 
-    // get the background color:
-    const bgColor = inputColor.value
-	// Set the color 
-	display.style.bgColor = bgColor
-    // Show the color
-    showBgColor.innerHTML = bgColor
+  // get the background color:
+  const bgColor = inputColor.value;
+  // Set the color
+  display.style.bgColor = bgColor;
+  // Show the color
+  showBgColor.innerHTML = bgColor;
 }
